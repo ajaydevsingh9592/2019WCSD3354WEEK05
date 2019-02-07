09fb46a6d7233997b079c85e63e788602568253c
visualstudio <visualstudio@230-PC40>
2019-02-07 14:46:54 -0500

    2:05

--- 2019WCSD3354WEEK05/Program.cs
+++ 2019WCSD3354WEEK05/Program.cs
@@ -10,6 +10,31 @@ namespace _2019WCSD3354WEEK05
     {
         static void Main(string[] args)
         {
+            var a = new testques2();
+            a.Play();
+        }
+    }
+
+    class testques2
+    {
+        public int myfavvari = 0;
+        public void Play()
+        {
+            //write a for loop
+
+            for (; my();)
+            {
+                if (myfavvari <= 10)
+                    Console.WriteLine("you are fool");
+                break;
+            }
+            Console.WriteLine("shit for loop again");
+        }
+        public bool my()
+        {
+            myfavvari++;
+            return true;
         }
     }
 }
+
